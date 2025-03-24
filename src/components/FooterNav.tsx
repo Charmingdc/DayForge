bbc1990e4e6f52189c0d4b5e42c10ffb549ea543
Adebayo Muis <162116910+Charmingdc@users.noreply.github.com>
2025-03-24 18:21:34 +0000

    Enhance FooterNav component: Implement todo form for adding new todos, improve state management for input values, and adjust layout styles

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { BsTrash3 } from "react-icons/bs";
 import { IoCheckmarkDoneOutline, IoAdd } from "react-icons/io5";
+import { SlArrowDownCircle } from "react-icons/sl";
 import { TodoProps } from "../types";
 
 type FooterProps = {
@@ -11,22 +13,31 @@ type FooterProps = {
 }
 
 const FooterNav: React.FC<FooterProps> = ({ setTodos, selectedTodos, setSelectedTodos, addTodo, deleteTodo }) => {
+ const [showTodoForm, setShowTodoForm] = useState<boolean>(false);
+ const [todoTextVal, setTodoTextVal] = useState<string>('');
+ const [todoTimeVal, setTodoTimeVal] = useState<'today' | 'someday'>('today');
+
 
  const handleTodoDeletion = (): void => {
   deleteTodo(selectedTodos);
   setSelectedTodos([]);
  }
 
- const handleAddNewTodo = () => {
+ const handleAddNewTodo = (e: React.FormEvent) => {
+  setShowTodoForm(false);
+
+  if (!todoTextVal.trim()) {
+    alert('Empty text');
+	return
+  }
+
   addTodo({
 	id: Math.floor(Math.random() * 1000),
-	todoText: 'New Todo',
-	todoTime: 'today',
+	todoText: todoTextVal,
+	todoTime: todoTimeVal,
 	isCompleted: false
   });
-  alert('Added');
  }
-
 
  const markSelectedAsCompleted = () => {
    setTodos((prevTodos: TodoProps[]) => 
@@ -44,13 +55,40 @@ const FooterNav: React.FC<FooterProps> = ({ setTodos, selectedTodos, setSelected
 	 <BsTrash3 size={24} />
 	</button>
 
-	<button onClick={handleAddNewTodo}>
+	<button onClick={() => setShowTodoForm(true)}>
 	  <IoAdd size={24} />
 	</button>
 
     <button onClick={markSelectedAsCompleted}>
 	  <IoCheckmarkDoneOutline size={24} />
 	</button>
+
+
+	{ showTodoForm && 
+	 <form className="todo-form" onSubmit={handleAddNewTodo}>
+	  <button type="button" onClick={() => setShowTodoForm(false)}>
+	   <SlArrowDownCircle />
+	  </button>
+
+	  <input
+	    type='text'
+		placeholder='What are we adding?'
+		value={todoTextVal}
+		onChange={(e) => setTodoTextVal(e.target.value)} />
+	  
+	  <button type="button">
+	   { todoTimeVal }
+	  </button>
+
+	  <ul>
+	   <li onClick={() => setTodoTimeVal('today')}> today </li>
+	   <li onClick={() => setTodoTimeVal('someday')}> someday </li>
+	  </ul>
+
+	  <button type="submit">
+		Add Todo
+	  </button>
+	 </form>}
   </section>
  );
 };
